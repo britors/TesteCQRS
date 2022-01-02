29f0f191f34646f1939718e4b15b17804605d579
Rodrigo Brito <britors@users.noreply.github.com>
2022-01-02 17:37:19 -0300

    Correção de mapeamento de objetos pro automapper

--- TesteCQRS.Application/ApplicationConfiguration.cs
+++ TesteCQRS.Application/ApplicationConfiguration.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using TesteCQRS.Application.Commands;
-using TesteCQRS.Application.Queries;
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using TesteCQRS.Application.Commands.Customer;
+using TesteCQRS.Application.Domain;
+using TesteCQRS.Application.Queries.Customer;
 
 namespace TesteCQRS.Application
 {
@@ -8,8 +10,14 @@ namespace TesteCQRS.Application
     {
         public static void MapperObjects(IServiceCollection services)
         {
-            CommandsConfiguration.MapperObjects(services);
-            QueriesConfiguration.MapperObjects(services);
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<CustomerCreateCommand, CustomerEntity>();
+                cfg.CreateMap<CustomerUpdateCommand, CustomerEntity>();
+                cfg.CreateMap<CustomerFindByIdQuery, CustomerEntity>();
+            });
+            IMapper mapper = config.CreateMapper();
+            services.AddSingleton(mapper);
         }
     }
 }
