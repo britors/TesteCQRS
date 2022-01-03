6a036beea31b3c90f333de192138a16c0c6b4f0f
Rodrigo Brito <britors@users.noreply.github.com>
2022-01-03 17:33:30 -0300

    Retornar data de criação de um processo

--- TesteCQRS.Application/Commands/QueueCommandResponse.cs
+++ TesteCQRS.Application/Commands/QueueCommandResponse.cs
@@ -7,6 +7,7 @@ namespace TesteCQRS.Application.Commands
     {
         public Guid CorrelationId { get; set; }
         public string ProcessName { get => GetType().Name; }
+        public DateTime CreatedAt { get => DateTime.Now; }
         public ProcessStatusEnum ProcessStatus { get; set; }
     }
 }
